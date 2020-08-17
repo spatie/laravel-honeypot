1186524e3a1e03a7debd521dd6248d8a5c364b3a
timmoh <timo@doersching.net>
2020-08-17 12:39:16 +0200

    add option to skip timestamp check

--- src/ProtectAgainstSpam.php
+++ src/ProtectAgainstSpam.php
@@ -9,23 +9,21 @@ use Illuminate\Support\Str;
 use Spatie\Honeypot\SpamResponder\SpamResponder;
 use Symfony\Component\HttpFoundation\Response;
 
-class ProtectAgainstSpam
-{
+class ProtectAgainstSpam {
+
     /** @var \Spatie\Honeypot\SpamResponder\SpamResponder */
     protected $spamResponder;
 
-    public function __construct(SpamResponder $spamResponder)
-    {
+    public function __construct(SpamResponder $spamResponder) {
         $this->spamResponder = $spamResponder;
     }
 
-    public function handle(Request $request, Closure $next): Response
-    {
-        if (! config('honeypot.enabled')) {
+    public function handle(Request $request, Closure $next): Response {
+        if (!config('honeypot.enabled')) {
             return $next($request);
         }
 
-        if (! $request->isMethod('POST')) {
+        if (!$request->isMethod('POST')) {
             return $next($request);
         }
 
@@ -35,55 +33,53 @@ class ProtectAgainstSpam
             $nameFieldName = $this->getRandomizedNameFieldName($nameFieldName, $request->all());
         }
 
-        if (! $this->shouldCheckHoneypot($request, $nameFieldName)) {
+        if (!$this->shouldCheckHoneypot($request, $nameFieldName)) {
             return $next($request);
         }
 
-        if (! $request->has($nameFieldName)) {
+        if (!$request->has($nameFieldName)) {
             return $this->respondToSpam($request, $next);
         }
 
         $honeypotValue = $request->get($nameFieldName);
 
-        if (! empty($honeypotValue)) {
+        if (!empty($honeypotValue)) {
             return $this->respondToSpam($request, $next);
         }
 
         $validFrom = $request->get(config('honeypot.valid_from_field_name'));
 
-        if (! $validFrom) {
+        if (!$validFrom) {
             return $this->respondToSpam($request, $next);
         }
 
-        try {
-            $time = new EncryptedTime($validFrom);
-        } catch (Exception $decryptException) {
-            $time = null;
-        }
+        if (config('honeypot.valid_from_timestamp')) {
+            try {
+                $time = new EncryptedTime($validFrom);
+            } catch (Exception $decryptException) {
+                $time = null;
+            }
 
-        if (! $time || $time->isFuture()) {
-            return $this->respondToSpam($request, $next);
+            if (!$time || $time->isFuture()) {
+                return $this->respondToSpam($request, $next);
+            }
         }
-
         return $next($request);
     }
 
-    private function getRandomizedNameFieldName($nameFieldName, $requestFields): ?String
-    {
+    private function getRandomizedNameFieldName($nameFieldName, $requestFields): ?string {
         return collect($requestFields)->filter(function ($value, $key) use ($nameFieldName) {
             return Str::startsWith($key, $nameFieldName);
         })->keys()->first();
     }
 
-    protected function respondToSpam(Request $request, Closure $next): Response
-    {
+    protected function respondToSpam(Request $request, Closure $next): Response {
         event(new SpamDetected($request));
 
         return $this->spamResponder->respond($request, $next);
     }
 
-    private function shouldCheckHoneypot(Request $request, ?string $nameFieldName): bool
-    {
+    private function shouldCheckHoneypot(Request $request, ?string $nameFieldName): bool {
         if (config('honeypot.honeypot_fields_required_for_all_forms') == true) {
             return true;
         }
