c150ff663f08cca157d7dab9d1d4327260e6953b
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-03-23 11:08:25 +0100

    Set tabindex to -1 for fields

--- resources/views/honeypotFormFields.blade.php
+++ resources/views/honeypotFormFields.blade.php
@@ -5,7 +5,7 @@
         }
     </style>
     <div id="{{ $nameFieldName }}_wrap" class="__d-n">
-        <input name="{{ $nameFieldName }}" type="text" value="" id="{{ $nameFieldName }}">
-        <input name="{{ $validFromFieldName }}" type="text" value="{{ $encryptedValidFrom }}">
+        <input name="{{ $nameFieldName }}" type="text" value="" id="{{ $nameFieldName }}" tabindex="-1">
+        <input name="{{ $validFromFieldName }}" type="text" value="{{ $encryptedValidFrom }}" tabindex="-1">
     </div>
 @endif
