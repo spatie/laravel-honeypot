a69cbd3dd6b02a176fa9dffea84b3182e9867ac4
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-03-24 09:05:16 +0100

    Revert using style tag to hide honeypot

--- resources/views/honeypotFormFields.blade.php
+++ resources/views/honeypotFormFields.blade.php
@@ -1,10 +1,5 @@
 @if($enabled)
-    <style>
-        .__d-n {
-            display: none;
-        }
-    </style>
-    <div id="{{ $nameFieldName }}_wrap" class="__d-n">
+    <div id="{{ $nameFieldName }}_wrap" style="opacity: 0; position: absolute; left: -9000px">
         <input name="{{ $nameFieldName }}" type="text" value="" id="{{ $nameFieldName }}" autocomplete="nope" tabindex="-1">
         <input name="{{ $validFromFieldName }}" type="text" value="{{ $encryptedValidFrom }}" autocomplete="nope" tabindex="-1">
     </div>
