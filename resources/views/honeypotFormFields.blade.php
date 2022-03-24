f01d6fba84c746739fa3cde4593cdaa7831ecc9f
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-03-24 09:09:02 +0100

    Use display: none and aria-hidden Probably less watertight to spammers but safer to use in every webpage (and with screenreaders)

--- resources/views/honeypotFormFields.blade.php
+++ resources/views/honeypotFormFields.blade.php
@@ -1,5 +1,5 @@
 @if($enabled)
-    <div id="{{ $nameFieldName }}_wrap" style="opacity: 0; position: absolute; left: -9000px">
+    <div id="{{ $nameFieldName }}_wrap" style="display: none" aria-hidden="true">
         <input name="{{ $nameFieldName }}" type="text" value="" id="{{ $nameFieldName }}" autocomplete="nope" tabindex="-1">
         <input name="{{ $validFromFieldName }}" type="text" value="{{ $encryptedValidFrom }}" autocomplete="nope" tabindex="-1">
     </div>
