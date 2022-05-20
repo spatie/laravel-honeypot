2029eee1fc05beb6d27f0641e2515dc89cd49099
Luca Longo <l.longo@ambita.it>
2022-05-20 12:45:39 +0200

    Livewire trait to handle spam requests

---
+++ src/Http/Livewire/Concerns/UsesSpamProtection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Spatie\Honeypot\Http\Livewire\Concerns;
+
+use Livewire\Component;
+use ReflectionProperty;
+use Spatie\Honeypot\Exceptions\SpamException;
+use Spatie\Honeypot\SpamProtection;
+
+/** @mixin Component */
+trait UsesSpamProtection
+{
+    public function guessHoneypotDataProperty(): ?HoneypotData
+    {
+        $props = (new \ReflectionClass($this))
+            ->getProperties(ReflectionProperty::IS_PUBLIC);
+
+        foreach ($props as $prop) {
+            if ($prop->getType()?->getName() === HoneypotData::class) {
+                return $prop->getValue($this);
+            }
+        }
+
+        return null;
+    }
+
+    protected function protectAgainstSpam(): void
+    {
+        $honeypotData = $this->guessHoneypotDataProperty();
+
+        if (is_null($honeypotData)) {
+            throw new \Exception("Livewire component requires a `HoneypotData` property.");
+        }
+
+        try {
+            app(SpamProtection::class)->check($honeypotData->toArray());
+        } catch (SpamException) {
+            abort(403, 'Spam detected.');
+        }
+    }
+}
